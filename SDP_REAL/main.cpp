2e628b5e9ec2a3d511be54a7f351372216393970
voidsoup292831 <tang.2226@buckeyemail.osu.edu>
2025-12-01 18:34:05 -0500

    Add files via upload
    The files I am currently using V1

---
+++ SDP_REAL/main.cpp
@@ -0,0 +1,28 @@
+#include "FEHLCD.h"
+#include "FEHUtility.h"
+
+class Main Menu
+{
+private:
+    /* data */
+public:
+ Main Menu(/* args */);
+     Main Menu();
+};
+{
+
+
+}
+;
+
+
+
+int main() {
+    // Clear background
+    LCD.SetBackgroundColor(BLACK);
+    LCD.Clear();
+
+   
+    
+    return 0;
+}
